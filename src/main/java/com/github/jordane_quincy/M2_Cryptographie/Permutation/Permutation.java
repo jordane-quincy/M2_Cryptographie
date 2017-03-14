5d8a36a2d705f902a8399657ad1fa658cd32ed10
jordane-quincy <jordanequincy@gmail.com>
2017-03-14 09:24:39 +0100

    permutation : ajout decode

--- src/main/java/com/github/jordane_quincy/M2_Cryptographie/Permutation/Permutation.java
+++ src/main/java/com/github/jordane_quincy/M2_Cryptographie/Permutation/Permutation.java
@@ -9,22 +9,26 @@ public class Permutation {
 
 	private static final Logger LOG = LogManager.getLogger(Permutation.class);
 
-	private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-	public static String encode(String sourceText, String newAlphabet) {
-		return encodeDecode(sourceText, newAlphabet, true);
+	public static String decode(String encodedText, String oldAlphabet, String newAlphabet) {
+		return encodeDecode(encodedText, oldAlphabet, newAlphabet, false);
 	}
 
-	public static String encodeDecode(String sourceTextOriginal, String newAlphabetOriginal, boolean encode) {
+	public static String encode(String sourceText, String oldAlphabet, String newAlphabet) {
+		return encodeDecode(sourceText, oldAlphabet, newAlphabet, true);
+	}
+
+	public static String encodeDecode(String sourceTextOriginal, String oldAlphabetOriginal, String newAlphabetOriginal,
+			boolean encode) {
 		final StringBuilder sb = new StringBuilder();
 		final String sourceText = Util.removeAllNonAlphabeticalCharacters(sourceTextOriginal);
 		final String newAlphabet = Util.removeAllNonAlphabeticalCharacters(newAlphabetOriginal);
+		final String oldAlphabet = Util.removeAllNonAlphabeticalCharacters(oldAlphabetOriginal);
 		LOG.info("sourceText : " + sourceText);
 
 		for (final char letter : sourceText.toCharArray()) {
 
 			// On retrouve l'index de la lettre à coder dans l'alphabet clair
-			final int indexLetter = ALPHABET.indexOf(letter);
+			final int indexLetter = oldAlphabet.indexOf(letter);
 			final char letterConverted = newAlphabet.substring(indexLetter, indexLetter + 1).charAt(0);
 
 			LOG.debug("letter '" + letter + "' : '" + letterConverted + "'");
--- src/test/java/com/github/jordane_quincy/M2_Cryptographie/Permutation/PermutationTest.java
+++ src/test/java/com/github/jordane_quincy/M2_Cryptographie/Permutation/PermutationTest.java
@@ -7,11 +7,25 @@ import junit.framework.Assert;
 public class PermutationTest {
 
 	@Test
-	public void testCM1_slide10() {
+	public void testCM1_slide10_decode() {
+		final String sourceText = "YVBSYSCSYSES";
+
+		final String oldAlphabet = "rgecvjuaspoimbwdtzxfhyklnq";
+		final String newAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+		final String encodedText = Permutation.decode(sourceText, oldAlphabet, newAlphabet);
+
+		Assert.assertEquals("VENIVIDIVICI", encodedText);
+	}
+
+	@Test
+	public void testCM1_slide10_encode() {
 		final String sourceText = "Veni, Vidi, Vici";
+
+		final String oldAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		final String newAlphabet = "rgecvjuaspoimbwdtzxfhyklnq";
 
-		final String encodedText = Permutation.encode(sourceText, newAlphabet);
+		final String encodedText = Permutation.encode(sourceText, oldAlphabet, newAlphabet);
 
 		Assert.assertEquals("YVBSYSCSYSES", encodedText);
 	}
